d8d957725575b1fcd9869bcccb4d5bd13bf34e7a
Sir_Mr_Bman <sirmrbman@gmail.com>
2013-12-04 17:30:01 -0500

    Fixed something else.

--- Better Broadcast/src/me/bman/betterbcast/main/BetterBcast.java
+++ Better Broadcast/src/me/bman/betterbcast/main/BetterBcast.java
@@ -7,7 +7,9 @@ package me.bman.betterbcast.main;
  */
 
 import java.util.logging.Logger;
+
 import me.bman.betterbcast.command.*;
+import me.bman.betterbcast.yaml.YamlConfigValues;
 
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -26,6 +28,9 @@ public class BetterBcast extends JavaPlugin {
 			
 			this.logger.info("[BetterBroadcast] Finished loading BetterBroadcast");
 			getCommand("bb").setExecutor(new Command_bb());
+			
+			YamlConfigValues.plugin = this;
+			Command_bb.plugin = this;
 		} catch ( Exception e ) {
 			this.logger.severe("[BetterBroadcast] BetterBroadcast failed to load!");
 			this.logger.severe("[BetterBroadcast] Now disabling BetterBroadcast...");
@@ -35,6 +40,8 @@ public class BetterBcast extends JavaPlugin {
 	
 	@Override
 	public void onDisable() {
+		YamlConfigValues.plugin = null;
+		Command_bb.plugin = null;
 		PluginDescriptionFile pdfFile = this.getDescription();
 		this.logger.info("[BetterBroadcast] BetterBroadcast by Sir_Mr_Bman version " + pdfFile.getVersion() + " has been disabled.");
 	}
