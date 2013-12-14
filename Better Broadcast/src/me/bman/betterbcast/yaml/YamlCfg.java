dc8e6b82a19ddb2c1d08b6998d555d1f8cf4cd75
Bman <sirmrbman@gmail.com>
2013-12-14 14:22:19 -0500

    Edited YAMLCFG again

--- Better Broadcast/src/me/bman/betterbcast/yaml/YamlCfg.java
+++ Better Broadcast/src/me/bman/betterbcast/yaml/YamlCfg.java
@@ -7,23 +7,23 @@ public class YamlCfg {
 		YamlCfg.plugin = plugin;
 	}
 	// Boolean: use custom permissions
-		public static boolean customPerm = plugin.getConfig().getBoolean("use-perm");
+		public boolean customPerm = plugin.getConfig().getBoolean("use-perm");
 		
 		// Boolean: display message
-		public static boolean dispMsg = plugin.getConfig().getBoolean("disp-message");
+		public boolean dispMsg = plugin.getConfig().getBoolean("disp-message");
 		
 		// String: custom permissions
-		public static String prefPerm = plugin.getConfig().getString("pref-perm");
+		public String prefPerm = plugin.getConfig().getString("pref-perm");
 		
-		public static String errPrefix = plugin.getConfig().getString("err-prefix");
+		public String errPrefix = plugin.getConfig().getString("err-prefix");
 		
 		// String: broadcast prefix
-		public static String bcastPrefix = plugin.getConfig().getString("bcast-prefix");
+		public String bcastPrefix = plugin.getConfig().getString("bcast-prefix");
 		
 		// String: No arguments message
-		public static String noArgs = plugin.getConfig().getString("no-args");
+		public String noArgs = plugin.getConfig().getString("no-args");
 		
 		// String: No permissions
-		public static String noPerms = plugin.getConfig().getString("no-perm");
+		public String noPerms = plugin.getConfig().getString("no-perm");
 		
 }
